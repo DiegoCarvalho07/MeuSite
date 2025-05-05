c496adf8e149d3145bb3bea9714712ca8a00745f
DiegoCarvalho07 <diegodecarvalho_@hotmail.com>
2025-05-05 12:15:40 -0300

    Add files via upload

---
+++ botao.js
@@ -0,0 +1,6 @@
+const botao = document.querySelector("#botao");
+
+botao.addEventListener("click",e =>{
+mensagem.innerHTML='"Disciplina é fazer o que você odeia, mas fazendo como se amasse."'
+
+});
